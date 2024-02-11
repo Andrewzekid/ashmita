e34b3e182d013dd45fceb303a4d939d79c3fd29d
Andrewzekid <79450923+Andrewzekid@users.noreply.github.com>
2024-02-11 21:18:16 +0900

    Delete public directory

--- public/joke2.js
+++
@@ -1,6 +0,0 @@
-const answerBtn = document.querySelector(".answer-btn1")
-const questionText = document.querySelector(".question")
-answerBtn.addEventListener("onclick", () => {
-    questionText.innerHTML = "The Paulice"
-})
-
