7a442d2a2b43c09327e1da9f079495950e980fff
Legend Andre <andreportilla@gmail.com>
2024-02-11 20:55:01 +0900

    Initial commit

---
+++ joke2.js
@@ -0,0 +1,6 @@
+const answerBtn = document.querySelector(".answer-btn1")
+const questionText = document.querySelector(".question")
+answerBtn.addEventListener("onclick", () => {
+    questionText.innerHTML = "The Paulice"
+})
+
